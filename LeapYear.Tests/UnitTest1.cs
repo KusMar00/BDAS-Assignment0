42885242a502b88f2a854e34c7a2f981d172ad7b
KusMar00 <74660130+KusMar00@users.noreply.github.com>
2021-09-06 17:35:34 +0200

    implement IsLeapYear(), with unit-tests (#2)

--- LeapYear.Tests/UnitTest1.cs
+++ LeapYear.Tests/UnitTest1.cs
@@ -3,12 +3,78 @@ using Xunit;
 
 namespace LeapYear.Tests
 {
-    public class UnitTest1
+    public class IsLeapYearTests
     {
         [Fact]
-        public void Test1()
+        public void IsLeapYear_divBy4_4_returns_true()
         {
+            //Arrange
+            var year = 4;
+            //Act
+            Program p = new Program();
+            bool output = p.IsLeapYear(year);
+            //Assert
+            Assert.True(output);
+        }
 
+        [Fact]
+        public void IsLeapYear_divBy4_503_returns_false()
+        {
+            //Arrange
+            var year = 503;
+            //Act
+            Program p = new Program();
+            bool output = p.IsLeapYear(year);
+            //Assert
+            Assert.False(output);
+        }
+
+        [Fact]
+        public void IsLeapYear_divBy4_2104_return_true()
+        {
+            //Arrange
+            var year = 2104;
+            //Act
+            Program p = new Program();
+            bool output = p.IsLeapYear(year);
+            //Assert
+            Assert.True(output);
+        }
+
+        [Fact]
+        public void IsLeapYear_divBy100_500_returns_false()
+        {
+            //Arrange
+            var year = 500;
+            //Act
+            Program p = new Program();
+            bool output = p.IsLeapYear(year);
+            //Assert
+            Assert.False(output);
+        }
+
+        [Fact]
+        public void IsLeapYear_divBy100_200_returns_false()
+        {
+            //Arrange
+            var year = 200;
+            //Act
+            Program p = new Program();
+            bool output = p.IsLeapYear(year);
+            //Assert
+            Assert.False(output);
+        }
+
+        [Fact]
+        public void IsLeapYear_divBy400_1600_returns_true()
+        {
+            //Arrange
+            var year = 1600;
+            //Act
+            Program p = new Program();
+            bool output = p.IsLeapYear(year);
+            //Assert
+            Assert.True(output);
         }
     }
 }
--- LeapYear/Program.cs
+++ LeapYear/Program.cs
@@ -2,11 +2,20 @@
 
 namespace LeapYear
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        }
+
+        public bool IsLeapYear(int year)
+        {
+            if (year%4==0)
+            {
+                if (year%100==0 && year%400!=0) return false;
+                else return true;
+            } 
+            return false;
         }
     }
 }
